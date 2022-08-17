8728e9a3f423ccaf9825a855232a494f8c360e89
Roberto A Moraes <endriosrobert@gmail.com>
2022-08-17 12:59:57 -0300

    Subindo o projeto com mongoDB

---
+++ src/main/java/com/poshyweb/workeshop/WorkeShopMongoApplication.java
@@ -0,0 +1,13 @@
+package com.poshyweb.workeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkeShopMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkeShopMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poshyweb/workeshop/WorkeShopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poshyweb.workeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkeShopMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
